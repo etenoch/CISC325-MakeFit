2c5044906fde6dced603322bc2f8c4c157e52779
Enoch <et.enoch@gmail.com>
2015-11-04 21:01:17 -0500

    + startscreen layout and calendar

---
+++ app/src/main/java/com/enochtam/cisc325/makefit/fragments/StartScreen.java
@@ -0,0 +1,88 @@
+package com.enochtam.cisc325.makefit.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.enochtam.cisc325.makefit.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class StartScreen extends Fragment implements OnDateSelectedListener, OnMonthChangedListener {
+
+
+    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
+
+
+    @Bind(R.id.calendar_view)
+    MaterialCalendarView calendarWidget;
+
+    @Bind(R.id.month)
+    TextView month;
+
+    View fragmentView;
+
+
+    Activity that;
+
+    public StartScreen() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        that = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        fragmentView = inflater.inflate(R.layout.fragment_start_screen, container, false);
+
+        ButterKnife.bind(this,fragmentView);
+        calendarWidget.setOnDateChangedListener(this);
+        calendarWidget.setOnMonthChangedListener(this);
+        return fragmentView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView calendarWidget, @Nullable CalendarDay date, boolean selected) {
+        month.setText(getSelectedDatesString());
+    }
+
+    @Override
+    public void onMonthChanged(MaterialCalendarView calendarWidget, CalendarDay date) {
+        //noinspection ConstantConditions
+//        getSupportActionBar().setTitle(FORMATTER.format(date.getDate()));
+    }
+
+    private String getSelectedDatesString() {
+        CalendarDay date = calendarWidget.getSelectedDate();
+        if (date == null) {
+            return "No Selection";
+        }
+        return FORMATTER.format(date.getDate());
+    }
+
+}
+
---
+++ app/src/main/java/com/enochtam/cisc325/makefit/fragments/WorkoutList.java
@@ -0,0 +1,44 @@
+package com.enochtam.cisc325.makefit.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.enochtam.cisc325.makefit.R;
+
+public class WorkoutList extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private String mParam1;
+
+
+    public static WorkoutScreen newInstance(String param1, String param2) {
+        WorkoutScreen fragment = new WorkoutScreen();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WorkoutList() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_list, container, false);
+    }
+
+
+}
+
